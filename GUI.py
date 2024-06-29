1ea3ab29a77e9199f8917cadd70cec12bed09a27
minhtt03 <tthuminh12@gmail.com>
2024-06-29 21:31:35 +0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -3,6 +3,7 @@ from tkinter import filedialog, messagebox
 import json
 import customtkinter as ctk
 from core import generate_aes_key, encrypt_file_aes, decrypt_file_aes, generate_rsa_key_pair, encrypt_string_rsa, decrypt_string_rsa, calculate_sha1
+import os.path
 
 LARGEFONT =("Verdana", 35)
 HELVETICA = ("Helvetica", 20, "bold")
@@ -64,50 +65,44 @@ class EncryptionFrame(tk.Frame):
     def __init__(self,parent, controller):
         # Encrypt File Section
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Encrypt File")
-
+        
+        label = tk.Label(self, text="Encrypt File",font=LARGEFONT)
+        label.grid(row=0,column=2,padx = 10, pady = 10)
         self.controller = controller
 
-        
+        select_file_btn = tk.Button(self,text = "Select File",command=lambda: SelectFile.select_file(self))
+        select_file_btn.grid(row=1,column=0,padx=10,pady=10)
 
-        # self.encrypt_frame = tk.LabelFrame( text="Encrypt File", padx=10, pady=10)
-        # self.encrypt_frame.pack(padx=10, pady=10)
+        selected_file_label = tk.Label(self, text="No file selected")
+        selected_file_label.grid(row=1,column=1,padx=10,pady=10)
 
-        # self.select_file_btn = tk.Button(self.encrypt_frame, text="Select File", command=self.select_file)
-        # self.select_file_btn.grid(row=0, column=0, padx=10, pady=10)
-
-        # self.selected_file_label = tk.Label(self.encrypt_frame, text="No file selected")
-        # self.selected_file_label.grid(row=0, column=1, padx=10, pady=10)
-
-        # self.encrypt_file_btn = tk.Button(self.encrypt_frame, text="Encrypt File", command=self.encrypt_file)
-        # self.encrypt_file_btn.grid(row=1, column=0, columnspan=2, pady=10)
+        encrypt_file_btn = tk.Button(self, text="Encrypt File",command=lambda: Encrypt.encrypt_file(self))
+        encrypt_file_btn.grid(row=2,column=0,columnspan=2,pady=10)
 
         
-
-
-
 class DecryptionFrame(tk.Frame):
     def __init__(self,parent, controller):
         # Decrypt File Section
         tk.Frame.__init__(self, parent)
+
+        label = tk.Label(self, text="Decrypt File",font=LARGEFONT)
+        label.grid(row=0,column=2,padx = 10, pady = 10)
         self.controller = controller
-        # self.decrypt_frame = tk.LabelFrame( text="Decrypt File", padx=10, pady=10)
-        # self.decrypt_frame.pack(padx=10, pady=10)
 
-        # self.select_enc_file_btn = tk.Button(self.decrypt_frame, text="Select Encrypted File")
-        # self.select_enc_file_btn.grid(row=0, column=0, padx=10, pady=10)
+        select_en_file_btn = tk.Button(self,text = "Select Encrypted File",command=lambda: SelectFile.select_file(self))
+        select_en_file_btn.grid(row=1,column=0,padx=10,pady=10)
 
-        # self.selected_enc_file_label = tk.Label(self.decrypt_frame, text="No file selected")
-        # self.selected_enc_file_label.grid(row=0, column=1, padx=10, pady=10)
+        selected_file_label = tk.Label(self, text="No file selected")
+        selected_file_label.grid(row=1,column=1,padx=10,pady=10)
 
-        # self.select_key_file_btn = tk.Button(self.decrypt_frame, text="Select Private Key File")
-        # self.select_key_file_btn.grid(row=1, column=0, padx=10, pady=10)
+        select_key_file_btn = tk.Button(self, text="Select Key File",command=lambda: SelectFile.select_key_file(self))
+        select_key_file_btn.grid(row=2,column=0,padx=10,pady=10)
 
-        # self.selected_key_file_label = tk.Label(self.decrypt_frame, text="No key file selected")
-        # self.selected_key_file_label.grid(row=1, column=1, padx=10, pady=10)
+        selected_file_label = tk.Label(self, text="No file selected")
+        selected_file_label.grid(row=2,column=1,padx=10,pady=10)
 
-        # self.decrypt_file_btn = tk.Button(self.decrypt_frame, text="Decrypt File")
-        # self.decrypt_file_btn.grid(row=2, column=0, columnspan=2, pady=10)
+        decrypt_btn = tk.Button(self, text="Decrypt File",command=lambda: Decrypt.decrypt_file(self))
+        decrypt_btn.grid(row=3,column=0,padx=10,pady=10)
 
 
 class SelectFile:
@@ -121,15 +116,18 @@ class SelectFile:
         if file_path:
             self.selected_enc_file_label.config(text=file_path)
             self.enc_file_path = file_path
-
     def select_key_file(self):
         file_path = filedialog.askopenfilename()
         if file_path:
             self.selected_key_file_label.config(text=file_path)
             self.key_file_path = file_path
+    # def save_file(self):
+    #     file_path = filedialog.savefilelog()
+    #     if file_path:
+    #         self.selected_file_label.config(text=file_path)
+    #         self.file_path = file_path
 
-
-class EncryptFile:
+class Encrypt:
     def encrypt_file(self):
         if hasattr(self, 'file_path'):
             # Step b: Generate AES key and encrypt file
@@ -139,6 +137,10 @@ class EncryptFile:
             # Step c: Generate RSA key pair and encrypt AES key
             rsa_private_key, rsa_public_key = generate_rsa_key_pair()
             encrypted_aes_key = encrypt_string_rsa(aes_key, rsa_public_key)
+
+
+            # output encrypted aes key to screen
+            
             sha1_hash = calculate_sha1(rsa_private_key)
 
             # Step d: Save metadata
@@ -161,7 +163,7 @@ class EncryptFile:
             messagebox.showwarning("Encrypt File", "No file selected!")
 
 
-class DecryptFile:
+class Decrypt:
     def decrypt_file(self):
         if hasattr(self, 'enc_file_path') and hasattr(self, 'key_file_path'):
             # Step b: Read private key from file
